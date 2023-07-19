cf53708de75016385ccf57e8382df0878162b907
Thien Nguyen <thieennt@gmail.com>
2023-07-19 15:38:57 +0700

    Completed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,56 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 import "./styles.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <a href="https://www.facebook.com/">
+              <FaFacebook />
+            </a>
+          </span>
+
+          <span className="icon">
+            <a href="https://www.instagram.com/">
+              <FaInstagram />
+            </a>
+          </span>
+
+          <span className="icon">
+            <a href="https://www.twitter.com/">
+              <FaTwitter />
+            </a>
+          </span>
+
+          <span className="icon">
+            <a href="https://www.linkedin.com/">
+              <FaLinkedin />
+            </a>
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
