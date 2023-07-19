d60899bfdf2d3a0f4afc86ff7215098b3c6c5acf
Thien Nguyen <thieennt@gmail.com>
2023-07-19 16:33:34 +0700

    Done carousel section - switch tabs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+
 import "./styles.scss";
 
 const Home = () => {
   return (
     <div>
       <HeroBanner />
+
       <div style={{ height: 1000 }}></div>
+      <Trending />
     </div>
   );
 };
